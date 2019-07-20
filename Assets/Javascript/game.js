d410577c3efc7eec7405b25e675d12d2d65f10ef
rawrgoesaimee <rawrgoesaimee@hotmail.com>
2019-07-20 00:37:21 -0700

    Added the codes to the js, jquery with some final touches on the html and css codes.

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	let answer;
     let totalAdd = 0;
-    let wins = "Wins: ";
-	let losses = "Losses: ";
+    let wins = 0;
+	let losses = 0;
 	let crystal1Num;
 	let crystal2Num;
 	let crystal3Num;
-	let crystal4Num;
+    let crystal4Num;
 
 	function newNumbers() {
 		answer = Math.floor(Math.random() * 110) + 10;
@@ -20,29 +20,30 @@ $(document).ready(function() {
 		newNumbers();
 		totalScore = 0;
 		$("#giveNumber").text(answer);
-		$("#totaladd").text(totalAdd);
+		$("#totalAdd").text(totalAdd);
 		$("#apatiteCrystal").attr("data-crystalvalue", crystal1Num);
 		$("#bloodStone").attr("data-crystalvalue", crystal2Num);
 		$("#garnetCrystal").attr("data-crystalvalue", crystal3Num);
 		$("#pinkTourmaline").attr("data-crystalvalue", crystal4Num);
 		$("#win").text(wins);
 		$("#lose").text(losses);
-		$("#winOrLose").text("");
+		$("#numberTotal").text("");
 
 		// console.log(crystal1Num, crystal2Num, crystal3Num, crystal4Num);
 	}
 
 	function youWin() {
-		$("#winOrLose").text("YOU WIN!");
+		$("#numberTotal").text("YOU WIN!");
 		wins++;
-		$("#wins").text(wins);
+		$("#win").text(wins);
 	}
 
 	function youLose() {
-		$("#winOrLose").text("YOU LOSE");
+		$("#numberTotal").text("YOU LOSE");
 		losses++;
-		$("#losses").text(losses);
-	}
+		$("#lose").text(losses);
+    }
+    
 
 	newGame();
 
@@ -54,25 +55,32 @@ $(document).ready(function() {
 	});
 
 	// Function to add the crystal values together
-	$(".crystalimg").on("click", function() {
-		if (totalScore >= magicNumber) {
+	$(".crystals").on("click", function() {
+		if (totalAdd >= answer) {
 			return;
 		}
 
-		var crystalValue = $(this).attr("data-crystalvalue");
+		let crystalValue = $(this).attr("data-crystalvalue");
 		crystalValue = parseInt(crystalValue);
-		totalScore += crystalValue;
-		$("#totalScore").text(totalScore);
+		totalAdd += crystalValue;
+		$("#totalAdd").text(totalAdd);
 
-		if (totalScore === magicNumber) {
+		if (totalAdd === answer) {
 			youWin();
-		} else if (totalScore > magicNumber) {
+		} else if (totalAdd > answer) {
 			youLose();
 		}
 	});
 
-	$(".btn").on("click", function() {
+	$("#playAgain").on("click", function() {
 		newGame();
+    });
+
+    $("#playAgain").hover(function() {
+		$(this).css({opacity: 0.3});
+	},
+	function() {
+		$(this).css({opacity: 1});
 	});
 
 });
